31333709bb2ea1991c3fe65406c7d6ce929012ba
Eduardo <dudu.gremista2011@gmail.com>
2024-07-16 10:56:50 -0300

    primeira versão

---
+++ tinkercad.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+serial
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ white(true){
+  serial.println("adeus memoria");
+ }
+}
